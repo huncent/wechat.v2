912ad4222533813f824627e0208c61ab360c9852
Xu Wenjing <xu@xuwenjing.com>
2016-07-26 17:47:27 +0800

    增加关注状态和关注时间

--- mp/oauth2/userinfo.go
+++ mp/oauth2/userinfo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/chanxuehong/wechat.v2/internal/debug/api"
 	"github.com/chanxuehong/wechat.v2/oauth2"
@@ -22,12 +23,14 @@ const (
 )
 
 type UserInfo struct {
-	OpenId   string `json:"openid"`   // 用户的唯一标识
-	Nickname string `json:"nickname"` // 用户昵称
-	Sex      int    `json:"sex"`      // 用户的性别, 值为1时是男性, 值为2时是女性, 值为0时是未知
-	City     string `json:"city"`     // 普通用户个人资料填写的城市
-	Province string `json:"province"` // 用户个人资料填写的省份
-	Country  string `json:"country"`  // 国家, 如中国为CN
+	OpenId        string    `json:"openid"`   // 用户的唯一标识
+	Nickname      string    `json:"nickname"` // 用户昵称
+	Sex           int       `json:"sex"`      // 用户的性别, 值为1时是男性, 值为2时是女性, 值为0时是未知
+	Subscribe     int       `json:"subscribe"`
+	SubscribeTime time.Time `json:"subscribe_time"`
+	City          string    `json:"city"`     // 普通用户个人资料填写的城市
+	Province      string    `json:"province"` // 用户个人资料填写的省份
+	Country       string    `json:"country"`  // 国家, 如中国为CN
 
 	// 用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），
 	// 用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
